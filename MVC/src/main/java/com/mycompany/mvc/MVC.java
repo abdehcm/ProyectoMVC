89d9d78e0f19c092d0c15c4c04fb8b36c0b209d3
hachimabdel <abdelhachim@icloud.com>
2024-10-22 16:13:02 +0200

    Creado el proyecto

---
+++ MVC/src/main/java/com/mycompany/mvc/MVC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mvc;
+
+/**
+ *
+ * @author alumnadotarde
+ */
+public class MVC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
